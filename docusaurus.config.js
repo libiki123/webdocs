42a3d51b47b91c1ab3fda36a341cdb1cb24e38eb
DESKTOP-3T50F0G\Ethan <libiki123@gmail.com>
2024-11-29 15:00:09 +0700

    alpha version

---
+++ docusaurus.config.js
@@ -0,0 +1,165 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Ethan Docs",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "docs",
+          sidebarPath: "./sidebars.js",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docsIcon.png",
+      navbar: {
+        title: "Ethan Insights",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/docsIcon.png",
+        },
+        items: [
+          // Left side
+          {
+            label: "Git",
+            to: "/docs/git-docs/intro",
+            type: "docSidebar",
+            sidebarId: "gitSidebar",
+          },
+          {
+            label: "Unity",
+            to: "/docs/unity-docs/intro",
+            type: "docSidebar",
+            sidebarId: "unitySidebar",
+          },
+          // {
+          //   type: "dropdown",
+          //   label: "Docs",
+          //   position: "left",
+          //   items: [
+          //     {
+          //       label: "Unity",
+          //       type: "docSidebar",
+          //       sidebarId: "tutorialSidebar",
+          //     },
+          //     {
+          //       label: "Git",
+          //       type: "docSidebar",
+          //       sidebarId: "tutorialSidebar",
+          //     },
+          //   ],
+          // },
+
+          // Right side
+          {
+            type: "search",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          // {
+          //   title: "Docs",
+          //   items: [
+          //     {
+          //       label: "Tutorial",
+          //       to: "/docs/intro",
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: "Community",
+          //   items: [
+          //     {
+          //       label: "Stack Overflow",
+          //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+          //     },
+          //     {
+          //       label: "Discord",
+          //       href: "https://discordapp.com/invite/docusaurus",
+          //     },
+          //     {
+          //       label: "X",
+          //       href: "https://x.com/docusaurus",
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: "More",
+          //   items: [
+          //     {
+          //       label: "Blog",
+          //       to: "/blog",
+          //     },
+          //     {
+          //       label: "GitHub",
+          //       href: "https://github.com/facebook/docusaurus",
+          //     },
+          //   ],
+          // },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ethan Truong - Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+      algolia: {
+        appId: "YOUR_ALGOLIA_APP_ID", //TODO: Add later
+        apiKey: "YOUR_ALGOLIA_API_KEY",
+        indexName: "YOUR_INDEX_NAME",
+        contextualSearch: true, // Optional, enables contextual search
+        searchParameters: {}, // Optional, additional parameters
+      },
+    }),
+};
+
+export default config;
